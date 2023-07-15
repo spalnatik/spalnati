a8ebc8efce2e90a14026fe86e0c64bc896dbfd27
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-07-15 12:16:36 +0530

    Update ad.sh

--- ad.sh
+++ ad.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo 'append domain-search "contoso.com"' >> /etc/dhcp/dhclient.conf
-sed -i '/^\[main\]/a dhcp = dhclient' /etc/NetworkManager/NetworkManager.conf
+sudo echo 'append domain-search "contoso.com"' >> /etc/dhcp/dhclient.conf
+sudo sed -i '/^\[main\]/a dhcp = dhclient' /etc/NetworkManager/NetworkManager.conf
 
-systemctl restart network
+sudo systemctl restart network
 
-yum install -y adcli krb5-workstation nmap oddjob oddjob-mkhomedir realmd samba-common-tools sssd
+sudo yum install -y adcli krb5-workstation nmap oddjob oddjob-mkhomedir realmd samba-common-tools sssd
 
-realm discover contoso.com
+sudo realm discover contoso.com
 
-realm join -U $username contoso.com
+sudo realm join -U $username contoso.com
