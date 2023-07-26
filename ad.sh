6c1db829842d6b9a359d34f8f10dca2845174fa8
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-07-26 11:19:42 +0530

    Update ad.sh

--- ad.sh
+++ ad.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo echo 'append domain-search "contoso.com"' >> /etc/dhcp/dhclient.conf
+sudo echo 'append domain-search "intl.contoso.com"' >> /etc/dhcp/dhclient.conf
 sudo sed -i '/^\[main\]/a dhcp = dhclient' /etc/NetworkManager/NetworkManager.conf
 echo "nameserver 10.0.0.15" | sudo tee /etc/resolv.conf
 sudo systemctl restart network
