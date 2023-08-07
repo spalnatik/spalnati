b6673b8dc60d3d0b68729fa6afaf3ede534145e3
spalnatik <139609488+spalnatik@users.noreply.github.com>
2023-08-07 08:14:21 +0530

    Update suse.sh

--- suse.sh
+++ suse.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update the NETCONFIG_DNS_STATIC_SEARCHLIST value in the config file
-sed -i 's/NETCONFIG_DNS_STATIC_SEARCHLIST=.*/NETCONFIG_DNS_STATIC_SEARCHLIST="contoso.com"/' /etc/sysconfig/network/config
+sed -i 's/NETCONFIG_DNS_STATIC_SEARCHLIST=.*/NETCONFIG_DNS_STATIC_SEARCHLIST="intl.contoso.com"/' /etc/sysconfig/network/config
 
 # Restart the network service
 systemctl restart network
